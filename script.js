2439831382403e6aa7a97f9f27a2f1d7a6386ef7
Ihor Korobenko <korobenkoss@live.com>
2021-03-29 21:43:21 +0300

    Add drawing functionality

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const sketchpad = document.querySelector('.sketchpad');
+const buttons = document.querySelectorAll('.buttons');
 
-for(let i = 0; i < 16; i++){
-    const pixel = document.createElement('div');
-    pixel.style.cssText = 'height: 50px; width: 50px; border: 2px solid orange;'
-    sketchpad.appendChild(pixel);
+
+function drawPad(size=2){
+    document.querySelectorAll('.pixel').forEach(p => sketchpad.removeChild(p))
+
+    sketchpad.style.cssText = `grid-template-rows: repeat(${size}, 1fr);`
+    sketchpad.style.cssText = `grid-template-columns: repeat(${size}, 1fr);`
+    for(let i = 0; i < size * size; i++){
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        sketchpad.appendChild(pixel);
+    }
+
+    document.querySelectorAll('.pixel').forEach(pix => {
+        pix.addEventListener('mouseover', e => {
+            pix.style.cssText = 'background-color: black;'
+        })
+    })
 }
+
+drawPad();
+
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        let size = button.value;
+        drawPad(size);   
+        document.querySelectorAll('.pixel').forEach(pix => {
+            pix.addEventListener('click', e => {
+                pix.style.cssText = 'background-color: black;'
+            })
+        })
+    })
+})
+
+document.querySelector('#clear').addEventListener('click', e => {
+    document.querySelectorAll('.pixel').forEach(p => {
+        p.style.cssText = 'background-color: none';
+    })
+})
